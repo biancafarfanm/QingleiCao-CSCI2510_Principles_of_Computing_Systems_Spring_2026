792f4d73323be01f7f5fe3da553b3f2b5853614b
Bianca Farfan <biancafarfan@Biancas-MacBook-Pro.local>
2026-02-26 21:26:54 -0600

    Completed Studio Input Output

---
+++ copy.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+#define bufferSize 200
+
+int main() {
+
+    char buffer[bufferSize];
+    ssize_t bytesRead;
+
+    while (1) {
+
+        bytesRead = read(STDIN_FILENO, buffer, bufferSize);
+
+        if (bytesRead == 0) {
+            break;
+        }
+
+        write(STDOUT_FILENO, buffer, bytesRead);
+    }
+
+    return 0;
+}
